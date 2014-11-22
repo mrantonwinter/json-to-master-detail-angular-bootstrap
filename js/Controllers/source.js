e543b6e7244c2f0ef5f0a4b8b755ee96245e842e
mrantonwinter <sirantonwinter@gmail.com>
2014-11-22 14:23:33 +1100

    added user input

---
+++ js/Controllers/source.js
@@ -0,0 +1,10 @@
+﻿app.controller("source", function ($scope, $rootScope) {
+    $scope.Name = '';
+    $rootScope.JSON = '{"id":1,"name":"john smith","email":"john@gmail.com", "dob":"5/11/1968","groupID":10}';
+
+    $scope.Go = function ($event) {
+        $event.preventDefault();
+        $rootScope.JSON = JSON.stringify(JSON.parse($rootScope.JSON), null, 4);
+        $rootScope.$broadcast('Go');
+    }
+});
---
+++ js/app.js
@@ -0,0 +1,6 @@
+var app = angular.module("app", []);
+
+app.run(function ($rootScope) {
+    //$rootScope.TypeOptions = ['label', 'input', 'email', 'textarea', 'checkbox', 'select', 'datepicker'];
+
+});
